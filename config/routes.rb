f85cb9ccecbeca28cb2e76946a54ad4e7d626f7e
Bluesmile82 <pursuance@gmail.com>
2016-06-17 00:08:40 +0200

    Installs and configures devise_token_auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  mount_devise_token_auth_for 'User', at: 'auth'
   namespace :api do
     namespace :v1 do
       resources :mindmaps, only: [:index], defaults: { format: 'json' }
