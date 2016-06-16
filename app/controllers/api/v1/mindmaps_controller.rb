6235e660ca2cd0ea9d15245ca6ba10b4a4b427f4
Bluesmile82 <pursuance@gmail.com>
2016-06-17 00:09:14 +0200

    Authenticates user before any action

--- app/controllers/api/v1/mindmaps_controller.rb
+++ app/controllers/api/v1/mindmaps_controller.rb
@@ -2,6 +2,7 @@ module Api
   module V1
     class MindmapsController < ApplicationController
       http_basic_authenticate_with name: "admin", password: "trycatch"
+      before_action :authenticate_user!
 
       def index
         @mindmaps = Mindmap.all
