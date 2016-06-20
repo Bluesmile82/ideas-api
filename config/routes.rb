1ff1e4d363b6ff775ee0573e3e6385e084737d63
Bluesmile82 <pursuance@gmail.com>
2016-06-20 23:10:07 +0200

    Updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   namespace :api do
     namespace :v1 do
-      resources :mindmaps, only: [:index], defaults: { format: 'json' }
+      resources :mindmaps, only: [:index, :create, :show, :update, :destroy], defaults: { format: 'json' }
+      resources :ideas, only: [:index], defaults: { format: 'json' }
     end
   end
 end
